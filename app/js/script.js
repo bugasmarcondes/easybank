00d23454475dce8bd339c30765497ac2e4d0c5a0
Bruno Marcondes <bugasmarcondes@gmail.com>
2021-07-23 18:10:00 -0300

    Hamburguer animation + Overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,17 @@
-console.log('@ hello world')
+// @1
+// const header = document.querySelector('#hamburguer-btn')
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
+
+header.addEventListener('click', e => {
+  const isOpen = header.classList.contains('open')
+  if (!isOpen) {
+    header.classList.add('open')
+    overlay.classList.remove('fade-out')
+    overlay.classList.add('fade-in')
+  } else {
+    header.classList.remove('open')
+    overlay.classList.remove('fade-in')
+    overlay.classList.add('fade-out')
+  }
+})
