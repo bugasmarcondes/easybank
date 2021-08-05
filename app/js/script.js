fd3d562eea174410ffb11e341b91a9b9bfb1f6b4
Bruno Marcondes <bugasmarcondes@gmail.com>
2021-08-05 18:17:35 -0300

    Animated Mobile Menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,29 @@
 // @1
 // const header = document.querySelector('#hamburguer-btn')
+const body = document.querySelector('body')
 const header = document.querySelector('.header')
 const overlay = document.querySelector('.overlay')
+const fadeElems = document.querySelectorAll('.has-fade')
 
 header.addEventListener('click', e => {
   const isOpen = header.classList.contains('open')
   if (!isOpen) {
+    body.classList.add('noscroll')
     header.classList.add('open')
     overlay.classList.remove('fade-out')
     overlay.classList.add('fade-in')
+    fadeElems.forEach(element => {
+      element.classList.remove('fade-out')
+      element.classList.add('fade-in')
+    })
   } else {
+    body.classList.remove('noscroll')
     header.classList.remove('open')
     overlay.classList.remove('fade-in')
     overlay.classList.add('fade-out')
+    fadeElems.forEach(element => {
+      element.classList.remove('fade-in')
+      element.classList.add('fade-out')
+    })
   }
 })
